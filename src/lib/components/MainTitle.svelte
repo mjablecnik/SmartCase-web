ed1f987ae5e3ec2078b68f512cfd687d212ccb46
Martin Jablečník <martin.jablecnik@email.cz>
2023-01-12 16:16:26 +0100

    Add main title

---
+++ src/lib/components/MainTitle.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let thickText;
+  export let thinText;
+  export let description;
+</script>
+
+
+<p>
+<span class="thick-text-font"> {thickText} </span> <br/>
+<span class="thin-text-font"> {thinText} </span> <br/><br/>
+<span class="description-text-font"> {description} </span>
+</p>
+
+<style>
+  .thick-text-font {
+    font-family: 'Poppins';
+    @apply font-bold text-7xl text-cyan-400;
+  }
+  .thin-text-font {
+    font-family: 'Poppins';
+    @apply font-thin text-7xl text-cyan-400;
+  }
+  .description-text-font {
+    font-family: 'Poppins';
+    @apply font-light text-xl text-black;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
   import MainImage from '../lib/components/MainImage.svelte';
+  import MainTitle from '../lib/components/MainTitle.svelte';
   import Button from '../lib/components/Button.svelte';
 </script>
 
 <div class="container mx-auto block w-auto">
-  <MainImage />
+  <div class="grid grid-cols-2 p-8 items-center">
+    <MainTitle thickText="Marketingová řešení" thinText="na míru" description="Pomůžeme Vám rozjet Vaše podnikání!" />
+    <MainImage />
+  </div>
 </div>
 
 <style>
