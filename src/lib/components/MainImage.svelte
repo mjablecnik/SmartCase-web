bbf4027fdbc59288dbde0dbbd624218596be2aaf
Martin Jablečník <martin.jablecnik@email.cz>
2023-01-12 15:20:11 +0100

    Add contact into main image

--- src/lib/components/MainImage.svelte
+++ src/lib/components/MainImage.svelte
@@ -1,1 +1,29 @@
 <img src="/images/main-image.png"/>
+
+<!--
+<div class="relative w-auto h-auto">
+
+<img src="/images/main-image.png"/>
+
+<p class="text-font absolute position">
+KONTAKT
+<br><br>
+info@smartcase.cz<br /> 
++ 420 727 913 350
+</p>
+
+</div>
+
+
+<style>
+  .position {
+    position: absolute;
+    top: 17vh;
+    left: 41vw;
+  }
+  .text-font {
+    font-family: 'Poppins';
+    @apply font-light text-lg text-cyan-400 leading-6;
+  }
+</style>
+-->
