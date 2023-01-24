75a9af4b3b26b053a173c5eeda5ca265fa203af7
Martin Jablečník <martin.jablecnik@email.cz>
2023-01-24 17:19:23 +0100

    Add strategy section

---
+++ src/lib/sections/StrategySection.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Button from '../components/Button.svelte';
+</script>
+
+
+<img src="/images/strategy.png" class="md:hidden" />
+<div class="grid grid-cols-12 lg:grid-cols-2">
+  <div class="col-span-12 md:col-span-5 lg:col-span-1">
+    <div class="label-text pb-4"> NAŠE STRATEGIE </div>
+    <span class="thick-text-font"> Unikátní Marketingový </span> <br/>
+    <span class="thin-text-font"> plán </span> <br/><br/>
+    <span class="description-text-font"> 
+    Audit aktuální situace, podle které zjistíme Váš stav a vytvoříme plán Vám přímo na míru
+    </span>
+    <Button clazz="py-6 lg:py-8" padding="pl-3 pr-1.5 py-1">Více informací</Button>
+  </div>
+  <img src="/images/strategy.png" class="hidden md:block md:col-span-7 lg:col-span-1" />
+</div>
+
+
+<style>
+  .label-text {
+    font-family: 'Poppins';
+    @apply font-light text-base lg:text-lg text-cyan-400;
+  }
+  .thick-text-font {
+    font-family: 'Poppins';
+    @apply font-bold text-3xl md:text-4xl lg:text-5xl xl:text-7xl text-cyan-400;
+  }
+  .thin-text-font {
+    font-family: 'Poppins';
+    @apply font-thin text-3xl md:text-4xl lg:text-5xl xl:text-7xl text-cyan-400;
+  }
+  .description-text-font {
+    font-family: 'Poppins';
+    @apply font-light text-base md:text-base lg:text-base xl:text-xl text-black;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,12 @@
   import MainSection from '../lib/sections/MainSection.svelte';
   import ExpectationSection from '../lib/sections/ExpectationSection.svelte';
   import ClientsSection from '../lib/sections/Clients.svelte';
+  import StrategySection from '../lib/sections/StrategySection.svelte';
 </script>
 
 <div class="container mx-auto block w-auto">
   <div class="grid gap-12">
-    <div class="grid gap-12 px-8 px-16 items-start sm:ml-20 lg:ml-0 ">
+    <div class="grid gap-12 pl-8 px-16 items-start sm:ml-20 lg:ml-0">
       <MainSection />
     </div>
     <div class="md:px-10 lg:px-0 justify-self-center">
@@ -14,6 +15,9 @@
     </div>
     <div class="px-8 md:pl-4 md:pr-16 lg:pr-0 items-start sm:ml-20 lg:ml-0 lg:mt-16">
       <ClientsSection />
+    </div>
+    <div class="grid gap-12 px-16 md:px-8 items-start sm:ml-12 lg:ml-20 justify-self-center">
+      <StrategySection />
     </div>
   </div>
 </div>
